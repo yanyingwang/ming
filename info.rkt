d41febd8c84e0f2e12f7bc04ae9abfcd11528b6f
Yanying Wang <yanyingwang1@gmail.com>
2020-11-24 11:17:31 +0800

    $ raco pkg new ming-lang

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "ming-lang")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/ming-lang.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(yanying))
---
+++ scribblings/ming-lang.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[ming-lang
+                    racket/base]]
+
+@title{ming-lang}
+@author{yanying}
+
+@defmodule[ming-lang]
+
+Package Description Here
