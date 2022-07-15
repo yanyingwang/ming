4b0f03e4a214332016950de34683dc7b661ead9f
Yanying Wang <yanyingwang1@gmail.com>
2022-07-15 19:17:31 +0800

    refix ga error

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "ming")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "scribble-rainbow-delimiters"))
 (define scribblings '(("scribblings/ming.scrbl" ())))
 (define pkg-desc "名语言，中文化的LISP编程语言。")
 (define version "0.1")
