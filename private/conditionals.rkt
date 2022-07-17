fe514fafbc32e0b792fe064b357d2625208c6a16
Yanying Wang <yanyingwang1@gmail.com>
2022-07-17 20:08:57 +0800

    且或

--- private/conditionals.rkt
+++ private/conditionals.rkt
@@ -4,9 +4,11 @@
 (provide
  (rename-out
   ;; 如 若 倘 况
-  [if 若]
-  [case 倘]
-  [cond 当]
+  [if 若] ;; 若是
+  [case 倘] ;; 倘如
+  [cond 当] ;; 在……时候
   [else 否则]
 
+  [and 且] ;; 也 还 又 且
+  [or 或] ;; 不
   ))
