02d116de5afd3482a4cdbce4730c5c1d9fb002a6
Yanying Wang <yanyingwang1@gmail.com>
2022-07-31 00:06:28 +0800

    almost ready to use of core.rkt

--- core.rkt
+++ core.rkt
@@ -1,37 +1,54 @@
 #lang racket/base
 
-(provide minglize)
-(require (for-syntax racket/require-transform
-                     racket/base
-                     racket/syntax))
-(require (for-syntax (rename-in "mapping/racket.rkt" [table table-racket])))
-(require (for-syntax (rename-in "mapping/racket/base.rkt" [table table-racket/base])))
-(require (for-syntax (rename-in "mapping/racket/list.rkt" [table table-racket/list])))
+(provide minglize
+         (for-syntax table-racket
+                     table-racket/base
+                     table-racket/list))
 
-(define-syntax (minglize1 stx)
-  (datum->syntax stx
-                 '(require ming/core
-                           (for-syntax (rename-in "mapping/racket/list.rkt"
-                                                  [table table-racket/list])))))
+
+(require (for-syntax racket/base
+                     racket/syntax
+                     racket/require-transform))
+
+;; (define-syntax (mapping-init stx)
+;;   (datum->syntax stx '(require (for-syntax (rename-in ming/mapping/racket [table table-racket])
+;;                                            (rename-in ming/mapping/racket/base [table table-racket/base])
+;;                                            (rename-in ming/mapping/racket/list [table table-racket/list])))))
+
+(require  (for-syntax (rename-in "mapping/racket.rkt" [table minglizing-mapping--racket])
+                      (rename-in "mapping/racket/base.rkt" [table minglizing-mapping--racket/base])
+                      (rename-in "mapping/racket/list.rkt" [table minglizing-mapping--racket/list])))
+
 
 (define-for-syntax (get-table path)
-  (eval (format-id path "table-~a" path))
-  )
+  (eval (format-id path "minglizing-mapping--~a" path)))
 
 (define-syntax minglize
   (make-require-transformer
    (lambda (stx)
-     (displayln stx)
+     ;; (displayln stx)
      (syntax-case stx ()
        [(_ path)
-        (displayln #'path)
+        (displayln (format-id #'path "ming/mapping/~a" #'path))
         (expand-import
-         (datum->syntax stx `(rename-in ,#'path ,@(get-table #'path)))) ;; TODO: table should be defined in define syntax block depending on the value of #'path
+         (datum->syntax stx `(combine-in (for-syntax ,(format-id #'path "ming/mapping/~a" #'path))
+                                         (rename-in ,#'path ,@(get-table #'path)))))
         ]))))
-
-
 
 ;; usage
 ;; (require (minglize racket/list))  ;; minglize 名化，汉化 chineselize
-;; (甲 (list 1 2 3))
+;; (甲 (链 1 2 3))
 ;; (乙 (list 1 2 3))
+
+#|
+(require ming/core)
+(mapping-init)
+(begin-for-syntax (println table-racket))
+(require (minglize racket))
+
+(require  (for-syntax (rename-in "mapping/racket.rkt" [table table-racket])
+                      (rename-in "mapping/racket/base.rkt" [table table-racket/base])
+                      (rename-in "mapping/racket/list.rkt" [table table-racket/list])))
+
+
+|#
