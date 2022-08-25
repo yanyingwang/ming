b902c7a30eed2e1c09c5d71070a88483f08c8245
Yanying Wang <yanyingwang1@gmail.com>
2022-08-25 20:47:45 +0800

    fix useless code

--- private/mapping.rkt
+++ private/mapping.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(provide gen-mapping-data mapping-files)
+(provide gen-mapping-data)
 (require racket/runtime-path racket/string)
 
 (define (gen-mapping-data data #:scribble? [scribble? #f])
@@ -14,10 +14,3 @@
         (list a b c)
         (list a b)))
   )
-
-
-(define-runtime-path the-path "../mapping")
-(define mapping-files
-  (for/list ([f (in-directory the-path)]
-             #:when (string-suffix? (path->string f) ".rkt"))
-    (string-replace (path->string f) #rx".*/mapping" "")))
