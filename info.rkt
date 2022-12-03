41e96b95932131438060248c721f58f7e06aca73
Yanying Wang <yanyingwang1@gmail.com>
2022-12-04 00:10:32 +0800

    at-exp-lib

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "ming")
-(define deps '("base" "scribble-lib"))
+(define deps '("base" "scribble-lib" "at-exp-lib"))
 (define build-deps '("sandbox-lib" "scribble-doc" "scribble-lib" "racket-doc" "rackunit-lib" "scribble-rainbow-delimiters"))
 (define scribblings '(("scribblings/ming.scrbl" (multi-page))))
 (define pkg-desc "名语言，中文化的LISP编程语言。")
