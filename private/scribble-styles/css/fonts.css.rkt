72200f52e6bf8874f6ba70911dcf559311a0b0cd
Yanying Wang <yanyingwang1@gmail.com>
2023-04-01 21:15:12 +0800

    use base64 css file instead of regular woff2 font file

--- private/scribble-styles/css/fonts.css.rkt
+++ private/scribble-styles/css/fonts.css.rkt
@@ -6,8 +6,10 @@
          racket/runtime-path)
 (provide css/wenquan-extend-font)
 
+(define-runtime-path source-han-sans-cnx.css "source-han-sans-cnx.css")
 (define-runtime-path fonts.css "fonts.css")
 
 (define css/wenquan-extend-font
   (elem #:style (make-style #f
-                            (list (make-css-addition fonts.css)))))
+                            (list (make-css-addition source-han-sans-cnx.css)
+                                  (make-css-addition fonts.css)))))
