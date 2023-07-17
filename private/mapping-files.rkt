56e429eb4916ee509747a7dd2bcf0d860c1de24a
Yanying Wang <yanyingwang1@gmail.com>
2023-07-18 00:12:25 +0800

    use explicit defined mapping-files instead of finding from directory

--- private/mapping-files.rkt
+++ private/mapping-files.rkt
@@ -1,10 +1,50 @@
 #lang racket/base
 
-(provide mapping-files)
-(require racket/runtime-path racket/string)
+(provide mapping-files
+         mapping-files-racket/base
+         mapping-files-racket
+         mapping-files-ming)
 
-(define-runtime-path the-path "../mapping")
+
+(define mapping-files-racket
+  '("/racket/base.rkt"
+    "/racket/hash.rkt"
+    "/racket/keyword.rkt"
+    "/racket/list.rkt"
+    "/racket/math.rkt"
+    "/racket/string.rkt"
+    "/racket/symbol.rkt"
+    "/racket/vector.rkt"
+    "/racket.rkt")
+  )
+
+(define mapping-files-racket/base
+  '(
+    "/racket/base/box.rkt"
+    "/racket/base/characters.rkt"
+    "/racket/base/generic-numerics.rkt"
+    "/racket/base/hash.rkt"
+    "/racket/base/keywords.rkt"
+    "/racket/base/number-types.rkt"
+    "/racket/base/others.rkt"
+    "/racket/base/pairs-and-lists.rkt"
+    "/racket/base/procedures.rkt"
+    "/racket/base/string.rkt"
+    "/racket/base/symbols.rkt"
+    "/racket/base/syntactic-forms.rkt"
+    "/racket/base/syntax.rkt"
+    "/racket/base/vector.rkt"
+    )
+  )
+
+
+(define mapping-files-ming
+  '("/ming/core.rkt")
+  )
+
+
+;; all rkt files in mapping dir
 (define mapping-files
-  (for/list ([f (in-directory the-path)]
-             #:when (string-suffix? (path->string f) ".rkt"))
-    (string-replace (path->string f) #rx".*/mapping" "")))
+  (append mapping-files-ming mapping-files-racket mapping-files-racket/base)
+  )
+
